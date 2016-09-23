848c8e2d463343f217c1795e4fc15e7739c34910
José Laya <jmlaya@gmail.com>
2016-09-22 19:52:57 -0500

    Fix .env file issue

--- src/lib/env.js
+++ src/lib/env.js
@@ -1,8 +1,8 @@
 import module from 'module';
 import env    from 'node-env-file';
 
-if(!process.env.SB_PP_ENV){
-    env(__dirname + '/../../.env');
+if(!process.env.SB_PP_PORT){
+    env(__dirname + '/../../.env.default');
 }
 process.env.NODE_PATH = `${__dirname}/../`;
 module.Module._initPaths();
