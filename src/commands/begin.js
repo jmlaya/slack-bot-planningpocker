efd62948a821e7ed1c5b7965b937fe24464e7d3c
José Laya <jmlaya@gmail.com>
2016-09-22 20:08:43 -0500

    Test begin command

--- src/commands/begin.js
+++ src/commands/begin.js
@@ -1,5 +1,5 @@
 import slackResponse from 'lib/slack-response';
 
-export default (req, res, next, value) => {
+modeule.exports = (req, res, next, value) => {
     res.send(slackResponse('Begin command recived with: ' + value));
 };
